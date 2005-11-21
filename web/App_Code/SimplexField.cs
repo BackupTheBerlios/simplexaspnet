4a699fc51b83c52415a9864a465690a9cd148e95
simplexnet <simplexnet>
2005-11-21 11:34:55 +0000

    properties für 2 methoden

--- web/App_Code/SimplexField.cs
+++ web/App_Code/SimplexField.cs
@@ -2,44 +2,84 @@ using System.Web;
 using System.Web.Services;
 using System.Web.Services.Protocols;
 
-public abstract class SimplexField : Matrix
+public class SimplexField : Matrix
 {
 
-    int pivotRow=-1;
-    int pivotColumn=-1;
-   
+    #region -----------------------------------------------------------------------------
 
-    public  void AddOptimize(int[] indexes)
+    private int pivotRow = -1;
+    private int pivotColumn = -1;
+
+    #endregion --------------------------------------------------------------------------
+
+    
+
+    #region properties ------------------------------------------------------------------
+
+    public int PivotRow
+    {
+        get { return pivotRow; }
+        set { pivotRow = value; }
+    }
+
+    //-----------------------------------------------------------------------------------
+
+    public int PivotColumn
+    {
+        get { return pivotColumn; }
+        set { pivotColumn = value; }
+    }
+    
+    //-----------------------------------------------------------------------------------
+    
+    #endregion
+
+    #region methods ---------------------------------------------------------------------
+
+    public void AddOptimize(int[] indexes)
     {
     }
+
+    //-----------------------------------------------------------------------------------
 
     /// <summary>
     /// Adds a restriction to the matrix
     /// </summary>
-    public  void AddRestriction(int[] restFkt)
+    public void AddRestriction(int[] restFkt)
     {
     }
 
-    public  void AddSingleOptimize(int index)
+    //-----------------------------------------------------------------------------------
+
+    public void AddSingleOptimize(int index)
     {
     }
+
+    //-----------------------------------------------------------------------------------
 
     /// <summary>
     /// sets the function for the matrix
     /// </summary>
-    public  void SetAimFkt(int[] aimFkt)
+    public void SetAimFkt(int[] aimFkt)
     {
     }
+
+    //-----------------------------------------------------------------------------------
 
     /// <summary>
     /// defines the base variables in the first column of the matrix
     /// </summary>
-    public  void SetBaseVariable(int index)
+    public void SetBaseVariable(int index)
     {
     }
+
+    //-----------------------------------------------------------------------------------
 
     public void Method()
     {
         throw new System.NotImplementedException();
     }
+
+    #endregion --------------------------------------------------------------------------
+    
 }
