dfb41528cb0d01966c7461884bef14eb6b2205c4
sonite <sonite>
2005-12-02 15:26:58 +0000

    *** empty log message ***

---
+++ netzwerkfluss/NetwerkFluss/DotBuilder.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Matrix;
+
+   
+public class BuildDot
+{
+    private M matrix;
+    public BuildDot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+    public void Build()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
