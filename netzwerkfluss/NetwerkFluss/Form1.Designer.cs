1e8ccf4423330b102c1cb4b13eaa46658b21c964
simplexnet <simplexnet>
2005-12-05 12:40:56 +0000

    *** empty log message ***

--- netzwerkfluss/NetwerkFluss/Form1.Designer.cs
+++ netzwerkfluss/NetwerkFluss/Form1.Designer.cs
@@ -64,7 +64,7 @@ namespace NetwerkFluss
             // 
             this.button1.Location = new System.Drawing.Point(145, 65);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(117, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Matrix erstellen";
             this.button1.UseVisualStyleBackColor = true;
